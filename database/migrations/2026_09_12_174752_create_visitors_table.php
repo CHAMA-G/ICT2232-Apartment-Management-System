e4fb395cc0082ffbb820445457809c15185f19c2
Chamith Dhanuja <chamithdhanuja94@gmail.com>
2026-09-12 23:18:18 +0530

    Chamith added role to users and created visitors table migration

---
+++ database/migrations/2026_09_12_174752_create_visitors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('visitor_name');
+            $table->string('phone');
+            $table->string('vehicle_number')->nullable();
+            $table->string('flat_number');
+            $table->string('status')->default('Checked-In');
+            $table->timestamp('check_in_time')->nullable();
+            $table->timestamp('check_out_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
